127d9c98f7253d04ae939cfaa96a6fdd8acf968e
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2025-08-26 18:58:43 +0530

    implemented middleware , controller , routes,db connectio, user schhema

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/api/status",(req,res)=>res.send("Server is live"));
 app.use("/api/auth",userRouter);
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5000; 
 // connect db
 connectDb().then(
 server.listen(PORT,()=> console.log(`Server is running at port ${PORT}`))
